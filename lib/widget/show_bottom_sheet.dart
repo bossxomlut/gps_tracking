7344a39793a0ee8e0d4d78c48216feb68e272ebd
Vinh Ngo Tan <vinhnt@sendo.vn>
2024-01-10 22:00:51 +0700

    update ui menu
    #tool: listing feature

---
+++ lib/widget/show_bottom_sheet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+mixin ShowBottomSheet<T> on Widget {
+  Future<T?> showBottomSheet(BuildContext context) {
+    return showModalBottomSheet<T>(
+      context: context,
+      builder: (context) => SafeArea(child: this),
+      isScrollControlled: true,
+      useSafeArea: true,
+      enableDrag: !true,
+      backgroundColor: Colors.white,
+      shape: const RoundedRectangleBorder(),
+    );
+  }
+}
