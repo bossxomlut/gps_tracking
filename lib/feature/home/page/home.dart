5317686d2434013f1c7eba7266d682c820e3077b
Vinh Ngo Tan <vinhnt@sendo.vn>
2024-01-18 14:11:23 +0700

    refactor: separate downloader util file

--- lib/feature/home/page/home.dart
+++ lib/feature/home/page/home.dart
@@ -14,7 +14,7 @@ import 'package:mp3_convert/feature/home/cubit/home_cubit.dart';
 import 'package:mp3_convert/feature/home/cubit/home_state.dart';
 import 'package:mp3_convert/feature/home/data/entity/media_type.dart';
 import 'package:mp3_convert/feature/home/data/entity/setting_file.dart';
-import 'package:mp3_convert/feature/home/interface/pick_multiple_file.dart';
+import 'package:mp3_convert/feature/home/data/entity/pick_multiple_file.dart';
 import 'package:mp3_convert/feature/home/widget/file_type_widget.dart';
 import 'package:mp3_convert/feature/home/widget/menu_widget.dart';
 import 'package:mp3_convert/feature/home/widget/uploading_progress_bar.dart';
--- lib/main_setting/app_setting.dart
+++ lib/main_setting/app_setting.dart
@@ -1,9 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:mp3_convert/util/downloader_util.dart';
 
 class AppSetting {
   Future initApp() async {
     return Future.wait([
+      //init locale language
       EasyLocalization.ensureInitialized(),
+
+      //init downloader
+      FlutterDownloader.initialize(debug: true, ignoreSsl: true).whenComplete(() {
+        FlutterDownloader.registerCallback(downloadCallback);
+      }),
     ]);
   }
 }
