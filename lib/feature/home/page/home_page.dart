a3f65966623913f8da4a5660104bc9228f054753
vinhnt <kieuphong2840@gmail.com>
2024-01-21 22:04:53 +0700

    refactor ui home: add scroll behavior

--- lib/feature/home/page/home_page.dart
+++ lib/feature/home/page/home_page.dart
@@ -23,47 +23,68 @@ class MenuPage extends BasePage {
 
   @override
   Widget buildBody(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Features",
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          const SizedBox(height: 4),
-          Card(
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ConvertPage(),
-                ));
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        AppImage.svg(IconPath.exchange),
-                        const SizedBox(width: 16),
-                        Text(
-                          'Convert',
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                      ],
-                    )),
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Features",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 4),
+                  Card(
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ConvertPage(),
+                        ));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                        child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Row(
+                              children: [
+                                AppImage.svg(IconPath.exchange),
+                                const SizedBox(width: 16),
+                                Text(
+                                  'Convert',
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                              ],
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ];
+      },
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "History",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 4),
+                  const HistoryDownloadWidget(),
+                ],
               ),
             ),
           ),
-          const SizedBox(height: 20),
-          Text(
-            "History",
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          const SizedBox(height: 4),
-          const HistoryDownloadWidget(),
         ],
       ),
     );
@@ -99,124 +120,5 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MenuPage();
-  }
-}
-
-class HistoryDownloadWidget extends StatefulWidget {
-  const HistoryDownloadWidget({super.key});
-
-  @override
-  State<HistoryDownloadWidget> createState() => _HistoryDownloadWidgetState();
-}
-
-class _HistoryDownloadWidgetState extends AppLifeCycleMixin<HistoryDownloadWidget> {
-  @override
-  void onResume() {
-    super.onResume();
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<AppFile>>(
-      future: (() async {
-        final path = await getPath();
-        Directory dir = Directory(path);
-
-        return dir
-            .listSync()
-            .whereType<File>()
-            .map((f) => AppFile(
-                  path: f.absolute.uri.path,
-                  name: f.name,
-                ))
-            .toList();
-      })(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data != null) {
-          final files = snapshot.data!;
-          if (files.isEmpty) {
-            return Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  AppImage.asset(
-                    ImagePath.empty,
-                    width: 60,
-                    height: 60,
-                    color: Theme.of(context).hintColor,
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    "Empty list",
-                    style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                          color: Theme.of(context).hintColor,
-                        ),
-                  ),
-                ],
-              ),
-            );
-          }
-          return Column(
-            children: [...snapshot.data!.map((f) => FileInfoCard(file: f))],
-          );
-        }
-        return const SizedBox();
-      },
-    );
-  }
-}
-
-class FileInfoCard extends StatelessWidget {
-  const FileInfoCard({super.key, required this.file});
-
-  final AppFile file;
-
-  @override
-  Widget build(BuildContext context) {
-    final stat = FileStat.statSync(file.path);
-
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  Icons.file_present_rounded,
-                  size: 60,
-                ),
-                Text(file.type),
-              ],
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    file.name,
-                    style: Theme.of(context).textTheme.labelLarge,
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    stat.modified.toString(),
-                    style: Theme.of(context).textTheme.labelSmall,
-                  ),
-                ],
-              ),
-            ),
-            IconButton(
-                onPressed: () {
-                  Share.shareXFiles([XFile(file.path)]);
-                },
-                icon: Icon(Icons.share)),
-          ],
-        ),
-      ),
-    );
   }
 }
