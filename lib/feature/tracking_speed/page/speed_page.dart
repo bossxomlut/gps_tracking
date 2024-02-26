743bfe536660aad961001392c58a41518c1fed5f
vinhnt <kieuphong2840@gmail.com>
2024-02-26 17:07:08 +0700

    update ui

--- lib/feature/tracking_speed/page/speed_page.dart
+++ lib/feature/tracking_speed/page/speed_page.dart
@@ -5,6 +5,16 @@ import 'package:mp3_convert/feature/tracking_speed/cubit/tracking_cubit.dart';
 import 'package:mp3_convert/feature/tracking_speed/widgets/button.dart';
 import 'package:mp3_convert/feature/tracking_speed/widgets/cycling_background.dart';
 
+extension TimeFormat on Duration {
+  String getMinuteFormat() {
+    int sec = inSeconds % 60;
+    int min = (inSeconds / 60).floor();
+    String minute = min.toString().length <= 1 ? "0$min" : "$min";
+    String second = sec.toString().length <= 1 ? "0$sec" : "$sec";
+    return '$minute’ $second‘’';
+  }
+}
+
 class SpeedPage extends StatefulWidget {
   const SpeedPage({super.key});
 
@@ -34,37 +44,32 @@ class _SpeedPageState extends BasePageState<SpeedPage> {
   @override
   Widget buildBody(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            child: CyclingBackground(
-              child: Center(
-                child: BlocSelector<PositionTrackingMovingCubit, TrackingMovingState, double>(
-                  selector: (state) => state.currentSpeed,
-                  builder: (context, speed) {
-                    return Text(
-                      "$speed",
-                      maxLines: 1,
-                      style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                            fontSize: 180,
-                          ),
-                    );
-                  },
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: CyclingBackground(
+                child: Center(
+                  child: BlocSelector<PositionTrackingMovingCubit, TrackingMovingState, double>(
+                    selector: (state) => state.currentSpeed,
+                    builder: (context, speed) {
+                      return Text(
+                        "${speed.toStringAsFixed(0)}",
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                              fontSize: 180,
+                            ),
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-          StreamBuilder(
-              stream: trackingMovingCubit.timerStream,
-              builder: (c, d) {
-                if (d.hasData) {
-                  return Text(d.toString());
-                }
-                return const SizedBox();
-              }),
-          _MovingInfo(),
-          _MovingController(),
-        ],
+            _MovingInfo(),
+            _MovingController(),
+          ],
+        ),
       ),
     );
   }
@@ -75,22 +80,67 @@ class _MovingInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PositionTrackingMovingCubit, TrackingMovingState>(builder: (context, state) {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Column(
-            children: [Text("Avarage"), Text("${state.speed.averageSpeed}")],
-          ),
-          Column(
-            children: [Text("Distance"), Text("${state.distance.totalDistance}")],
-          ),
-          Column(
-            children: [Text("Max Speed"), Text("${state.speed.maxSpeed}")],
-          ),
-        ],
-      );
-    });
+    final theme = Theme.of(context);
+
+    return Container(
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: theme.highlightColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: BlocBuilder<PositionTrackingMovingCubit, TrackingMovingState>(builder: (context, state) {
+        return Column(
+          children: [
+            Row(
+              children: [
+                Text("Start at: ${DateTime.now()}"),
+                StreamBuilder(
+                    stream: context.read<PositionTrackingMovingCubit>().timerStream,
+                    builder: (c, d) {
+                      if (d.hasData) {
+                        return Text("Duration: ${d.data?.getMinuteFormat()}");
+                      }
+                      return const SizedBox();
+                    }),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: _TitleAndContent(
+                    title: "Avarage",
+                    content: "${state.speed.averageSpeed.toStringAsFixed(1)}",
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  width: 1,
+                  color: Colors.white12,
+                ),
+                Expanded(
+                  child: _TitleAndContent(
+                    title: "Distance",
+                    content: "${state.distance.totalDistance.toStringAsFixed(1)}",
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  width: 1,
+                  color: Colors.white12,
+                ),
+                Expanded(
+                  child: _TitleAndContent(
+                    title: "Max Speed",
+                    content: "${state.speed.maxSpeed.toStringAsFixed(1)}",
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      }),
+    );
   }
 }
 
@@ -111,12 +161,14 @@ class _MovingController extends StatelessWidget {
 
           case InProgressTrackingMovingState():
             return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 PauseButton(
                   onTap: () {
                     context.read<PositionTrackingMovingCubit>().pause();
                   },
                 ),
+                const SizedBox(width: 16),
                 StopButton(
                   onTap: () {
                     context.read<PositionTrackingMovingCubit>().stop();
@@ -126,12 +178,14 @@ class _MovingController extends StatelessWidget {
             );
           case PauseTrackingMovingState():
             return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 PlayButton(
                   onTap: () async {
                     context.read<PositionTrackingMovingCubit>().resume();
                   },
                 ),
+                const SizedBox(width: 16),
                 StopButton(
                   onTap: () {
                     context.read<PositionTrackingMovingCubit>().stop();
@@ -149,6 +203,31 @@ class _MovingController extends StatelessWidget {
             );
         }
       },
+    );
+  }
+}
+
+class _TitleAndContent extends StatelessWidget {
+  const _TitleAndContent({super.key, required this.title, required this.content});
+
+  final String title;
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Column(
+      children: [
+        Text(
+          title,
+          style: textTheme.labelLarge,
+        ),
+        const SizedBox(height: 4),
+        Text(
+          content,
+          style: textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
